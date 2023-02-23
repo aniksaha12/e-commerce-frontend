db1a3ef00843e7f6e92de3348de20810ea85adc1
Anik <aniksaha859@gmail.com>
2023-02-23 13:21:43 +0530

    'added'

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -4,7 +4,9 @@ import axios from "axios";
 
 import * as actionTypes from '../constants/productConstant'
 
-const URL = 'http://localhost:8000';
+//const URL = 'http://localhost:8000';
+
+const URL = 'https://mybazzarapi.onrender.com/';
 
 export const getProducts = () => async(dispatch) => {
     try {
